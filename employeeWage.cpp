828a7a88c7ece8490e59417ba41f20cafb53f6b1
Ankit <ankitupadhyay1051@gmail.com>
2024-12-23 14:12:27 +0530

    UC1- Employee-attendance

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+
 int main(){
-    cout << "Welcome to the Employee Wage Computation" <<endl;
-    return 0;
+
+    cout << "Welcome to Employee-Wage-Computation" << endl;
+    
+     srand(time(0));
+
+        int randombit = rand() % 2; 
+        
+    if(randombit == 0){
+        cout << "Employee is Absent" << endl;;
+    }else{
+         cout << "Employee is Present";
+        }
+
+return 0;
 }
+
+
